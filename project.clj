c7bb0c69985feaca368b6af0ffbfa3c3a178e052
Rick Suijkerbuijk <rick.suijkerbuijk@jcore.com>
2023-06-20 03:59:00 +0200

    back to the start, trying to create a reflections file

--- project.clj
+++ project.clj
@@ -23,10 +23,7 @@
   :aliases {"kaocha" ["run" "-m" "kaocha.runner"]}
   :native-image {:name "metamorph"
                  :opts ["--verbose" "--no-fallback" "-H:ReflectionConfigurationFiles=META-INF/native-image/reflect-config.json"
-                        "--allow-incomplete-classpath"
-                        "--initialize-at-build-time=com.fasterxml.jackson.core,com.fasterxml.jackson.dataformat"
-                        "--initialize-at-run-time=clojure.core.cache.wrapped__init,clojure.core.cache__init,clojure.core.reducers__init,clojure.core.rrb_vector.interop__init"
-                        "--trace-class-initialization=clojure.core.cache__init,clojure.core.rrb_vector.interop__init,clojure.core.reducers__init"]}
+                        "--initialize-at-build-time=com.fasterxml.jackson.core,com.fasterxml.jackson.dataformat"]}
   :main ^:skip-aot metamorph.core
   :profiles {:uberjar {:aot :all}
              :dev {:dependencies [[lambdaisland/kaocha "1.69.1069"]
